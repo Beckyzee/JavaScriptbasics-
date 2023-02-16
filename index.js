ea7e72390356e3b4e2a73a0b11edb40cfa5b25e6
Beckyzee <zinabel7@gmail.com>
2023-02-16 17:30:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+//function that multiplies two numbers
+function multiplies (number1, number2) {
+   return number1 * number2
+}
+
+console.log(multiplies(20, 5));
+
+//variables
+let Firstname = 'Rebeccah';
+let Lastname = 'Abel';
+let Track = 'Frontend Development'
+let Profession = 'Front end Engineer';
+let Skills = 'HTML, css, JavaScript';
+let Color = 'Purple';
+let Independence = '1960';
+
+console.log("My name is " + Firstname +" " + Lastname + ". I am in the " + Track + " track of Stutern. My skills include but are not limited to " + Skills + ". My favorite color is " + Color + ". I am from Nigeria and my Country had her independence in " + Independence + ".");
